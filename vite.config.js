89c26fd91b62d216ed8062395b06530e696326b5
dineshkarthick21 <ssdineshkarthick@gmail.com>
2026-07-09 23:51:44 +0530

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './', // <-- Add this line right here
+  base: '/', // <-- Add this line right here
   plugins: [react(), tailwindcss()],
 })
